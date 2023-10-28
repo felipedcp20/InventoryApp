1f8b3266b9c1d394a41724ee733e0a35b08e4136
feli rodriguez <felipeandres.rodriguez@mercadolibre.com.co>
2023-10-27 21:09:20 -0500

    credentials .env

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,5 +1,9 @@
 import mysql.connector
 from fastapi import HTTPException
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class Dao:
@@ -8,11 +12,11 @@ class Dao:
 
     def connect_to_db(self) -> mysql.connector.MySQLConnection:
         connection = mysql.connector.connect(
-            user="root",
-            password="root",
-            host="localhost",
-            port="3306",
-            database="inventario",
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_DATABASE"),
         )
 
         return connection
